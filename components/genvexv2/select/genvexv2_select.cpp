325f7a7de226a98ccbcd47008014c3ba7d6a29ab
Joachim Nybo <15981279+Jogge@users.noreply.github.com>
2025-11-25 21:10:20 +0100

    Update genvexv2_select.cpp

--- components/genvexv2/select/genvexv2_select.cpp
+++ components/genvexv2/select/genvexv2_select.cpp
@@ -1,55 +1,189 @@
-#include "genvexv2_select.h"
+#include "genvexv2_climate.h"
 #include "esphome/core/log.h"
+#include "esphome/core/helpers.h"
 
 namespace esphome {
 namespace genvexv2 {
+static const char *TAG = "genvexv2.climate";
 
-static const char *TAG = "genvexv2.select";
+void Genvexv2Climate::setup() {
+  current_temp_sensor_->add_on_state_callback([this](float state) {
+    ESP_LOGD(TAG, "CURRENT TEMP SENSOR CALLBACK: %f", state);
+    current_temperature = state;
+    publish_state();
+  });
+  temp_setpoint_number_->add_on_state_callback([this](float state) {
+    ESP_LOGD(TAG, "TEMP SETPOINT SENSOR CALLBACK: %f", state);
+    target_temperature = state;
+    publish_state();
+  });
+  fan_speed_number_->add_on_state_callback([this](float state) {
+    ESP_LOGD(TAG, "FAN SPEED SENSOR CALLBACK: %f", state);
+    genvexv2fanspeed_to_fanmode(state);
+    publish_state();
+  });
 
-using modbus_controller::ModbusCommandItem;
-using modbus_controller::ModbusRegisterType;
-
-void Genvexv2Select::parse_and_publish(const std::vector<uint8_t> &data) {
-  union {
-    float float_value;
-    uint32_t raw;
-  } raw_to_float;
-
-  float received_value = payload_to_float(data, *this);
-  ESP_LOGD(TAG, "Genvexv2 Select index: %f", received_value);
-
-  auto options = traits.get_options();
-
-  if(received_value < options.size()) {
-    auto select_value = options[received_value];
-    ESP_LOGD(TAG, "Select new state : %s", select_value.c_str());
-    this->publish_state(select_value);
-  }
+  current_temperature = current_temp_sensor_->state;
+  target_temperature  = temp_setpoint_number_->state;
+  genvexv2fanspeed_to_fanmode(fan_speed_number_->state);
+  publish_state();
 }
 
-void Genvexv2Select::control(const std::string &value) {
-  ESP_LOGD(TAG, "Genvexv2 Select state: %s", value.c_str());
+void Genvexv2Climate::control(const climate::ClimateCall& call) {
+  if (call.get_target_temperature().has_value())
+  {
+    this->target_temperature = *call.get_target_temperature();
+    float target = target_temperature;
+    ESP_LOGD(TAG, "Target temperature changed to: %f", target);
+    temp_setpoint_number_->make_call().set_value(target).perform();//set(target);
+  }
 
-  auto options = traits.get_options();
+  if (call.get_mode().has_value())
+  {
+    ESP_LOGD("TAG", "Mode changed from Home Assistant");
+    auto new_mode = *call.get_mode();
+    mode = new_mode;
+    switch (mode) {
+      case climate::CLIMATE_MODE_OFF: 
+      {
+        ESP_LOGD("TAG", "Mode changed to OFF");
 
-  for(auto i = 0; i<options.size(); ++i) {
-    if(options[i] == value) {
-      ESP_LOGD(TAG, "WRITING INDEX: %d", i);
-      std::vector<uint16_t> data = modbus_controller::float_to_payload(i, modbus_controller::SensorValueType::U_WORD);
-      uint16_t speed = i;
-      ESP_LOGD(TAG, "UINT16_t speed: %i", speed);
-      //auto write_cmd = ModbusCommandItem::create_write_multiple_command(modbus_controller_, this->start_address + this->offset, this->register_count, data);
-      auto write_cmd = ModbusCommandItem::create_write_single_command(modbus_controller_, this->start_address, speed);
+        this->set_fan_mode_(climate::CLIMATE_FAN_OFF);
+        this->clear_custom_fan_mode_();
 
-      // publish new value
-      write_cmd.on_data_func = 
-      [this, write_cmd, value](ModbusRegisterType register_type, uint16_t start_address, const std::vector<uint8_t> &data) {
-        // gets called when the write command is ack'd from the device
-        modbus_controller_->on_write_register_response(write_cmd.register_type, start_address, data);
-        this->publish_state(value);
-      };
-      modbus_controller_->queue_command(write_cmd);
+        ESP_LOGD(TAG, "Custom Fan mode set to: 0");
+        fan_speed_number_->make_call().set_value(0).perform();//set(0);
+        break;
+      }
+      case climate::CLIMATE_MODE_HEAT_COOL:
+      {
+        ESP_LOGD("TAG", "Mode changed to HEAT_COOL");
+        this->set_custom_fan_mode_("2");
+
+        auto optional_genvexv2_fan_mode = parse_number<float>("2");
+        if(optional_genvexv2_fan_mode.has_value())
+        {
+          auto genvexv2_fan_mode = optional_genvexv2_fan_mode.value();
+          ESP_LOGD(TAG, "Custom Fan mode set to: %i", static_cast<int>(genvexv2_fan_mode));
+          fan_speed_number_->make_call().set_value(genvexv2_fan_mode).perform();;//set(genvexv2_fan_mode);
+        }
+        break;
+      }
+      default:
+        break;
     }
+  }
+
+  if (call.get_fan_mode().has_value())
+  {
+    ESP_LOGD("TAG", "Fan mode changed to OFF from Home Assistant");
+
+    this->clear_custom_fan_mode_();
+    this->set_fan_mode_(climate::CLIMATE_FAN_OFF);
+
+    ESP_LOGD(TAG, "Fan mode set to: 0");
+    fan_speed_number_->make_call().set_value(0).perform();//set(0);
+  }
+
+  if (call.has_custom_fan_mode())
+  {
+    ESP_LOGD("TAG", "Fan mode changed to custom fan mode 1-4 from Home Assistant");
+    const char *new_custom_fan_mode = call.get_custom_fan_mode();
+    if (new_custom_fan_mode == nullptr) {
+      return;
+    }
+
+    this->set_custom_fan_mode_(new_custom_fan_mode);
+    this->fan_mode.reset();
+
+    auto optional_genvexv2_fan_mode = parse_number<float>(new_custom_fan_mode);
+    if(optional_genvexv2_fan_mode.has_value())
+    {
+      auto genvexv2_fan_mode = optional_genvexv2_fan_mode.value();
+      ESP_LOGD(TAG, "Custom Fan mode set to: %i", static_cast<int>(genvexv2_fan_mode));
+      fan_speed_number_->make_call().set_value(genvexv2_fan_mode).perform();//set(genvexv2_fan_mode);
+    }
+  }
+  this->publish_state();
+}
+
+climate::ClimateTraits Genvexv2Climate::traits() {
+  auto traits = climate::ClimateTraits();
+
+  traits.set_supported_custom_fan_modes({
+    "1",
+    "2",
+    "3",
+    "4"
+  });
+
+  traits.set_supported_fan_modes({ 
+    climate::ClimateFanMode::CLIMATE_FAN_OFF 
+  });
+
+  traits.set_supported_modes({
+    climate::ClimateMode::CLIMATE_MODE_OFF,
+    climate::ClimateMode::CLIMATE_MODE_HEAT_COOL,
+   });
+
+  traits.set_supports_current_temperature(true);
+  traits.set_supports_two_point_target_temperature(false);
+  traits.set_visual_temperature_step(0.1);
+  traits.set_visual_min_temperature(5);
+  traits.set_visual_max_temperature(30);
+
+  return traits;
+}
+
+void Genvexv2Climate::dump_config() {
+  LOG_CLIMATE("", "Genvexv2 Climate", this);
+}
+
+void Genvexv2Climate::genvexv2fanspeed_to_fanmode(const int state)
+{
+  ESP_LOGD("TAG", "In genvexv2fanspeed_to_fanmode");
+  climate::ClimateFanMode return_value;
+  ESP_LOGD("TAG", "State is %i", state);
+
+  this->clear_custom_fan_mode_();
+  this->fan_mode.reset();
+
+  switch (state) {
+  case 1:
+    ESP_LOGD("TAG", "Case 1");
+    this->mode = climate::CLIMATE_MODE_HEAT_COOL;
+    this->set_custom_fan_mode_("1");
+    break;
+
+  case 2:
+    ESP_LOGD("TAG", "Case 2");
+    this->mode = climate::CLIMATE_MODE_HEAT_COOL;
+    this->set_custom_fan_mode_("2");
+    break;
+
+  case 3:
+    ESP_LOGD("TAG", "Case 3");
+    this->mode = climate::CLIMATE_MODE_HEAT_COOL;
+    this->set_custom_fan_mode_("3");
+    break;
+
+  case 4:
+    ESP_LOGD("TAG", "Case 4");
+    this->mode = climate::CLIMATE_MODE_HEAT_COOL;
+    this->set_custom_fan_mode_("4");
+    break;
+
+  case 0:
+    ESP_LOGD("TAG", "Case 0");
+    this->set_fan_mode_(climate::CLIMATE_FAN_OFF);
+    this->mode = climate::CLIMATE_MODE_OFF;
+    break;
+
+  default:
+    ESP_LOGD("TAG", "Case default");
+    this->set_fan_mode_(climate::CLIMATE_FAN_OFF);
+    this->mode = climate::CLIMATE_MODE_OFF;
+    break;
   }
 }
 
